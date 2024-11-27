222e6af98791b2067250e8d310f68ae66f46cb9c
Austin Heiman <5932099+atheiman@users.noreply.github.com>
2024-11-26 22:48:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,12 +15,14 @@ provider "aws" {
   }
 }
 
+data "aws_partition" "current" {}
+data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_organizations_organization" "current" {}
-data "aws_partition" "current" {}
 
 locals {
   partition = data.aws_partition.current.partition
+  region    = data.aws_region.current.name
   acct_id   = data.aws_caller_identity.current.account_id
   org       = data.aws_organizations_organization.current
 }
